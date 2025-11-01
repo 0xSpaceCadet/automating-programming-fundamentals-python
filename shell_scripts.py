5ad623b207de7ae4cd0d0baef9fd8c54d3e6b5fd
Kunal Sharma <sskunall@amazon.com>
2025-11-01 21:38:12 +0530

    Update Shell with L

--- shell_scripts.py
+++ shell_scripts.py
@@ -1,3 +1,15 @@
 print ('shell script' * 5) # repeat string manipulation
-m_name = input('iostream > ') # iostream input
+username = 'Mary'
+password = 'swordfish'
+if username == 'Mary':
+    print('Hello, Mary')
+    if password == 'swordfish': print('Access granted.')
+    else: print('Wrong password.')
+
+spam=0
+while spam < 2:
+    print('Cuda Stream Shell')
+    spam = spam + 1
+
+m_name = input('iostream > ')  # iostream input
 print(type(m_name))
