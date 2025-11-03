517e138b0aac5ca0a470f93dc8dea144d09c7fb6
Kunal Sharma <sskunall@amazon.com>
2025-11-03 21:34:36 +0530

    add iterativeches.py

--- interactivechess.py
+++ interactivechess.py
@@ -1,36 +1,5 @@
 import sys, copy
 
-STARTING_PIECES = {'a8': 'bR', 'b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
-'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR', 'a7': 'bP', 'b7': 'bP',
-'c7': 'bP', 'd7': 'bP', 'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
-'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ', 'e1': 'wK', 'f1': 'wB',
-'g1': 'wN', 'h1': 'wR', 'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
-'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'}
-
-for k, b in STARTING_PIECES.items():
-    print(k, b, end='')
-
-print(end='\n')
-print(*['a','b','c','d'])
-
-def print_chessboard(board):
-    squares = []
-    is_white_square = True
-    for y in '87654321':
-        for x in 'abcdefgh':
-            print(x, y, is_white_square)
-            if x + y in board.keys():
-                squares.append(board[x + y])
-            else:
-                if is_white_square:
-                    squares.append(WHITE_SQUARE)
-                else:
-                    squares.append(BLACK_SQUARE)
-            is_white_square = not is_white_square
-        is_white_square = not is_white_square
-
-    print(board.format(*squares))
-
 BOARD_TEMPLATE = """
     a    b    c    d    e    f    g    h
    ____ ____ ____ ____ ____ ____ ____ ____
@@ -62,4 +31,39 @@ BOARD_TEMPLATE = """
 WHITE_SQUARE = '||'
 BLACK_SQUARE = '  '
 
-print_chessboard(board=BOARD_TEMPLATE)
+
+STARTING_PIECES = {'a8': 'bR', 'b8': 'bN', 'c8': 'bB', 'd8': 'bQ',
+'e8': 'bK', 'f8': 'bB', 'g8': 'bN', 'h8': 'bR', 'a7': 'bP', 'b7': 'bP',
+'c7': 'bP', 'd7': 'bP', 'e7': 'bP', 'f7': 'bP', 'g7': 'bP', 'h7': 'bP',
+'a1': 'wR', 'b1': 'wN', 'c1': 'wB', 'd1': 'wQ', 'e1': 'wK', 'f1': 'wB',
+'g1': 'wN', 'h1': 'wR', 'a2': 'wP', 'b2': 'wP', 'c2': 'wP', 'd2': 'wP',
+'e2': 'wP', 'f2': 'wP', 'g2': 'wP', 'h2': 'wP'}
+
+for k, b in STARTING_PIECES.items():
+    print(k, b, end='')
+
+print(end='\n')
+print(*['a','b','c','d'])
+
+
+def print_chessboard(board):
+    print("\nPrinting chess board\n")
+    squares = []
+    is_white_square = True
+    for y in '87654321':
+        for x in 'abcdefgh':
+            # rint(x, y, is_white_square)
+            if str(x) + str(y) in board.keys():
+                squares.append(board[x + y]) # value in that coordinate
+                # for item in squares: print(item, end=' ')
+
+            else:
+                if is_white_square:
+                    squares.append(WHITE_SQUARE)
+                else:
+                    squares.append(BLACK_SQUARE)
+            is_white_square = not is_white_square
+        is_white_square = not is_white_square
+    print(BOARD_TEMPLATE.format(*squares))
+
+print_chessboard(STARTING_PIECES)
